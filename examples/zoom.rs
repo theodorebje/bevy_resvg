675febd112efb86c7025636a2a811c805b7ecdb4
Theodore Bjernhed <uik1475@edu.kunskapsskolan.se>
2026-01-24 13:38:12 +0100

    feat: add example for what happens when you zoom

---
+++ examples/zoom.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+use bevy_pancam::{DirectionKeys, PanCam, PanCamPlugin};
+use bevy_resvg::{
+    plugin::SvgPlugin,
+    raster::{asset::SvgRasterAsset, component::SvgRasterComponent},
+};
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, SvgPlugin, PanCamPlugin::default()))
+        .add_systems(Startup, (setup, setup_camera))
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera2d,
+        PanCam {
+            grab_buttons: vec![],
+            move_keys: DirectionKeys::NONE,
+            zoom_to_cursor: false,
+            ..default()
+        },
+    ));
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let svg: Handle<SvgRasterAsset> = asset_server.load("transparent.svg");
+    commands.spawn(SvgRasterComponent(svg));
+}
