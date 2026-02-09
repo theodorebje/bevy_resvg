3a0af3c82ae86b13f1ba7d8c02f61d2ddeaeadc3
Theodore Bjernhed <uik1475@edu.kunskapsskolan.se>
2026-02-09 19:44:37 +0100

    fix: properly handle all `AssetEvent`s
    - Replace single monolithic event handler system with dedicated handlers
      for each `AssetEvent` variant.
    - Add support for the `Removed` and `Unused` events.
    - Properly handle the `Modified` event, which didn't actually update the
      `Sprite before`
    
    NOTE: `Added` isn't actually handled, but it isn't supposed to either.
    We react to `LoadedWithDependencies` instead.
    Fix: #1

--- src/raster/plugin.rs
+++ src/raster/plugin.rs
@@ -13,40 +13,97 @@ impl Plugin for SvgRasterPlugin {
     fn build(&self, app: &mut App) {
         app.init_asset::<SvgFile>()
             .init_asset_loader::<SvgFileLoader>()
-            .add_systems(Update, spawn_svg_sprites);
+            .add_systems(
+                Update,
+                (handle_svg_loaded, handle_svg_modified, handle_svg_removed),
+            );
     }
 }
 
-/// Adds a [`Sprite`] to all [`Entities`](Entity) with an [`Svg`]
-/// that doesn't already have an associated [`Sprite`].
-fn spawn_svg_sprites(
+macro_rules! read_events {
+    ($svg_events:expr, $($asset_event:path) | +) => {
+        $svg_events.read().filter_map(|event| match event {
+            $($asset_event { id } => Some(*id)),+,
+            _ => None,
+        }).collect::<Vec<_>>()
+    };
+}
+
+/// Handles newly loaded [`SvgFile`]s by adding [`Sprite`] components to waiting
+/// entities. This responds to [`AssetEvent::LoadedWithDependencies`].
+fn handle_svg_loaded(
     mut commands: Commands,
     mut svg_events: MessageReader<AssetEvent<SvgFile>>,
     svg_assets: Res<Assets<SvgFile>>,
     mut images: ResMut<Assets<Image>>,
     query: Query<(Entity, &Svg), Without<Sprite>>,
 ) {
-    for event in svg_events.read() {
-        match event {
-            AssetEvent::LoadedWithDependencies { id } | AssetEvent::Modified { id } => {
-                // Find all entities waiting for this SVG
-                for (entity, raster_component) in &query {
-                    if raster_component.0.id() == *id
-                        && let Some(svg_asset) = svg_assets.get(*id)
-                    {
-                        let handle = images.add(svg_asset.0.clone());
+    let loaded_ids = read_events!(svg_events, AssetEvent::LoadedWithDependencies);
 
-                        commands.entity(entity).insert(Sprite::from_image(handle));
-                    }
+    for (entity, svg) in &query {
+        let id = svg.0.id();
+        if loaded_ids.contains(&id) {
+            if let Some(svg_file) = svg_assets.get(id) {
+                let image_handle = images.add(svg_file.0.clone());
+                commands
+                    .entity(entity)
+                    .insert(Sprite::from_image(image_handle));
+                debug!("Added `Sprite` for `{id}` to entity {entity:?}");
+            } else {
+                warn!("`{id}` reported as loaded, but not found in `Assets`");
+            }
+        }
+    }
+}
+
+/// Handles modified [`SvgFile`]s (e.g. through
+/// [hot-reloading](https://github.com/bevyengine/bevy/blob/main/examples/asset/hot_asset_reloading.rs))
+/// by updating existing [`Sprite`]s. This responds to [`AssetEvent::Modified`].
+fn handle_svg_modified(
+    mut svg_events: MessageReader<AssetEvent<SvgFile>>,
+    svg_assets: Res<Assets<SvgFile>>,
+    mut images: ResMut<Assets<Image>>,
+    mut query: Query<(&Svg, &mut Sprite)>,
+) {
+    let modified_ids = read_events!(svg_events, AssetEvent::Modified);
+
+    for (svg, mut sprite) in &mut query {
+        let id = svg.0.id();
+        if modified_ids.contains(&id) {
+            if let Some(svg_file) = svg_assets.get(id) {
+                // Update the image data in-place instead of creating new handle
+                if let Some(image) = images.get_mut(&sprite.image) {
+                    *image = svg_file.0.clone();
+                    debug!("Updated `Image` data for modified `{id}`");
+                } else {
+                    // Fallback: create new handle if old one is invalid
+                    sprite.image = images.add(svg_file.0.clone());
+                    debug!("Replaced `Handle<Image>` for modified `{id}`");
                 }
+            } else {
+                warn!("`{id}` reported as modified, but not found in `Assets`");
             }
-            AssetEvent::Added { id } => {
-                debug!("added SVG asset '{id}'");
-            }
-            other => {
-                // TODO: Handle more events
-                warn!("Unimplemented Event for SVG raster assets: '{other:#?}'");
-            }
+        }
+    }
+}
+
+/// Handles removed and unused [`SvgFile`]s by cleaning up associated [`Sprite`]
+/// components. This fires when an [`Asset`] is either explicitly removed from
+/// the asset system, or removed due to the last strong handle being dropped.
+/// This coresponds to [`AssetEvent::Removed`] and [`AssetEvent::Unused`],
+/// respectively.
+fn handle_svg_removed(
+    mut commands: Commands,
+    mut svg_events: MessageReader<AssetEvent<SvgFile>>,
+    query: Query<(Entity, &Svg), With<Sprite>>,
+) {
+    let removed_ids = read_events!(svg_events, AssetEvent::Removed | AssetEvent::Unused);
+
+    for (entity, svg) in query {
+        let id = svg.0.id();
+        if removed_ids.contains(&id) {
+            commands.entity(entity).remove::<Sprite>();
+            info!("Removed `Sprite` for `{id}` from entity {entity:?}");
         }
     }
 }
